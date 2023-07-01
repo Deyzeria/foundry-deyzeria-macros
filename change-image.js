f9dd975bfc2bfc0547b8b22a18dc1362f8daa153
Sinantrarion <49957461+Sinantrarion@users.noreply.github.com>
2023-07-01 20:22:09 +0300

    Update change-image.js
    Added a version which works with v9 as well.

--- change-image.js
+++ change-image.js
@@ -2,3 +2,8 @@ const imgA = "Image here";
 const imgB = "Image 2 here";
 const src = token.document.texture.src === imgA ? imgB : imgA;
 return token.document.update({"texture.src": src});
+
+// To have a version which works with version 9 Foundry, change the line 3 and 4 to the following lines
+let img = token.data.img;
+img = token.data.img=== imgA ? imgB : imgA;
+token.document.update({ img });
