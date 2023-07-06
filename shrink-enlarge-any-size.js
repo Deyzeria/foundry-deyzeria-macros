18655518a32009a513a97213d0ca55fd55566e06
Sinantrarion <49957461+Sinantrarion@users.noreply.github.com>
2023-07-06 19:05:43 +0300

    Update shrink-enlarge-any-size.js

--- shrink-enlarge-any-size.js
+++ shrink-enlarge-any-size.js
@@ -5,7 +5,7 @@ await token.document.update({width: newSize, height: newSize});
 
 // Add this to the code if you also want to change the actual size in the token. 
 // It works to medium, large, huge and gargantuan.
-const newSizeAr = ['sm', 'med', 'lg', 'huge', 'grg']
+const newSizeAr = ['sm', 'med', 'lg', 'huge', 'grg'];
 let objUpdate = new Object();
 objUpdate['system.traits.size'] = newSizeAr[newSize];
 
