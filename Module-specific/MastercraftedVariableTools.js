faa4613c8a5866741ce0e42b0691779a0ffbcc0b
Deyzeria <49957461+Deyzeria@users.noreply.github.com>
2024-04-19 17:58:59 +0300

    Update MastercraftedVariableTools.js

--- Module-specific/MastercraftedVariableTools.js
+++ Module-specific/MastercraftedVariableTools.js
@@ -1,3 +1,82 @@
+const arrayNames = {
+  alchemist: {
+    name: "Alchemist’s supplies",
+    icon: `<i class="fa-solid fa-flask-vial"></i>`
+  },
+  brewer: {
+    name: "Brewer’s supplies",
+    icon: `<i class="fa-solid fa-beer-mug-empty"></i>`
+  },
+  calligrapher: {
+    name: "Calligrapher’s supplies",
+    icon: `<i class="fa-solid fa-signature"></i>`
+  },
+  carpenter: {
+    name: "Carpenter’s tools",
+    icon: `<i class="fa-solid fa-tree"></i>`
+  },
+  cartographer: {
+    name: "Cartographer’s tools",
+    icon: `<i class="fa-solid fa-map"></i>`
+  },
+  cobbler: {
+    name: "Cobbler’s tools",
+    icon: `<i class="fa-solid fa-socks"></i>`
+  },
+  cook: {
+    name: "Cook’s utensils",
+    icon: `<i class="fa-solid fa-utensils"></i>`
+  },
+  glassblower: {
+    name: "Glassblower’s tools",
+    icon: `<i class="fa-solid fa-glass-water"></i>`
+  },
+  herb: {
+    name: "Herbalism Kit",
+    icon: `<i class="fa-solid fa-leaf"></i>`
+  },
+  jeweler: {
+    name: "Jeweler’s tools",
+    icon: `<i class="fa-solid fa-gem"></i>`
+  },
+  leatherworker: {
+    name: "Leatherworker’s tools",
+    icon: `<i class="fa-solid fa-rug"></i>`
+  },
+  mason: {
+    name: "Mason’s tools",
+    icon: `<i class="fa-solid fa-building"></i>`
+  },
+  painter: {
+    name: "Painter’s supplies",
+    icon: `<i class="fa-solid fa-paintbrush"></i>`
+  },
+  pois: {
+    name: "Poisoner’s Kit",
+    icon: `<i class="fa-solid fa-vial-virus"></i>`
+  },
+  potter: {
+    name: "Potter’s tools",
+    icon: `<i class="fa-solid fa-monument"></i>`
+  },
+  smith: {
+    name: "Smith’s tools",
+    icon: `<i class="fa-solid fa-hammer"></i>`
+  },
+  tinker: {
+    name: "Tinker’s tools",
+    icon: `<i class="fa-solid fa-screwdriver-wrench"></i>`
+  },
+  weaver: {
+    name: "Weaver’s tools",
+    icon: `<i class="fa-solid fa-shirt"></i>`
+  },
+  woodcarver: {
+    name: "Woodcarver’s tools",
+    icon: `<i class="fa-solid fa-tree"></i>`
+  }
+};
+
 const arrayTools = macroArgs[0].split(',');
 var object = {};
 
